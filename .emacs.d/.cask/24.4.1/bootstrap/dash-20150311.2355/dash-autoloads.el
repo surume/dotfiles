ab4928842e08d8761e4d2e5e050e92c120b1a9fa
surume <mail@sururne.com>
2015-03-22 21:03:33 +0900

    Add emacs.d

---
+++ .emacs.d/.cask/24.4.1/bootstrap/dash-20150311.2355/dash-autoloads.el
@@ -0,0 +1,15 @@
+;;; dash-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("dash.el") (21773 46094 833153 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; dash-autoloads.el ends here
---
+++ .emacs.d/.cask/24.4.1/bootstrap/package-build-20150318.2207/package-build-pkg.el
@@ -0,0 +1,1 @@
+(define-package "package-build" "20150318.2207" "Tools for assembling a package archive" '((cl-lib "0.5")) :keywords '("tools"))
