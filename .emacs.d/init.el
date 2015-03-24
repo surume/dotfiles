11e4f87bb6598966e3380ca2ceeb16179c1bb357
surume <mail@sururne.com>
2015-03-25 01:49:06 +0900

    Add emacs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -12,6 +12,13 @@
 (evil-mode 1)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; key-chord
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(require 'key-chord)
+(key-chord-mode 1)
+(setq key-chord-two-keys-delay 0.15)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; helm
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (require 'helm)
@@ -26,21 +33,14 @@
 (define-key helm-find-files-map (kbd "TAB") 'helm-execute-persistent-action)
 (define-key global-map (kbd "C-c i")   'helm-imenu)
 (setq helm-buffer-max-length 50)
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; helm-ghq
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (require 'helm-ghq)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; linum
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; 行No
-(global-linum-mode t)
-(setq linum-format "%2d ")
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; flycheck
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (require 'flycheck)
 (add-hook 'after-init-hook #'global-flycheck-mode)
 
@@ -67,12 +67,14 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (require 'git-gutter-fringe)
 
-
-
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; atom-dark
+;; popwin
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(load-theme 'atom-dark t)
+(require 'popwin)
+(setq display-buffer-function 'popwin:display-buffer)
+(setq popwin:popup-window-position 'bottom)
+;; helm
+(push '("*helm*") popwin:special-display-config)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; auto-save-buffers-enhanced
@@ -81,47 +83,136 @@
 (setq auto-save-buffers-enhanced-interval 1) ; 指定のアイドル秒で保存
 (auto-save-buffers-enhanced t)
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; evil-surround
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(require 'evil-surround)
+(global-evil-surround-mode 1)
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; rbenv
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(require 'rbenv)
+(global-rbenv-mode)
+(setq rbenv-installation-dir "/usr/local/var/rbenv")
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; ruby-block
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(require 'ruby-block)
+(ruby-block-mode t)
+(setq ruby-block-highlight-toggle t)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; ruby-mode
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(autoload 'ruby-mode "ruby-mode"
+  "Mode for editing ruby source files" t)
+(add-to-list 'auto-mode-alist '("\\.rb$latex " . ruby-mode))
+(add-to-list 'auto-mode-alist '("Capfile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Gemfile$" . ruby-mode))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; rcodetools
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;(add-to-list 'load-path "~/.emacs.d/rcodetools-0.8.5.0")
+;(require 'rcodetools)
+;(setq rct-find-tag-if-available nil)
+;(defun ruby-mode-hook-rcodetools ()
+;  (define-key ruby-mode-map "\M-\C-i" 'rct-complete-symbol)
+;  (define-key ruby-mode-map "\C-c\C-t" 'ruby-toggle-buffer)
+;  (define-key ruby-mode-map "\C-c\C-f" 'rct-ri))
+;(add-hook 'ruby-mode-hook 'ruby-mode-hook-rcodetools)
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; ruby-electric
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(require 'ruby-electric)
+(add-hook 'ruby-mode-hook '(lambda () (ruby-electric-mode t)))
+(setq ruby-electric-expand-delimiters-list nil)
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; atom-dark
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(load-theme 'atom-dark t)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; linum
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 行No
+(global-linum-mode t)
+(set-face-attribute 'linum nil
+                    :foreground "#636363"
+                    :background "#2E2E2E"
+                    :height 0.9)
+(setq linum-format "%3d ")
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; general-settings
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; スタートアップ非表示
 (setq inhibit-startup-screen t)
 ;; scratchの初期メッセージ消去
 (setq initial-scratch-message "")
 ;; ターミナルのときメニューを表示しない
 (if (eq window-system 'x)
-        (menu-bar-mode 1) (menu-bar-mode 0))
-
+    (menu-bar-mode 1) (menu-bar-mode 0))
 ;; 現在ポイントがある関数名をモードラインに表示
 (which-function-mode 1)
-
 ;; 対応する括弧をハイライト
 (show-paren-mode t)
-
 ;; yes-noをy-nに置き換え
 (fset 'yes-or-no-p 'y-or-n-p)
-
 ;; バックアップファイルを作らない
 (setq backup-inhibited t)
-
 ;; インデントはタブではなくスペースを使用
 (setq-default indent-tabs-mode nil)
-
 ;; 行末の空白をハイライト
 (setq-default show-trailing-whitespace t)
-
+;; タイトルバーにファイルのフルパス表示
+(setq frame-title-format
+      (format "%%f - Emacs@%s" (system-name)))
 ;; ファイル名補完で大文字小文字を区別しない
 (setq read-buffer-completion-ignore-case t)
 (setq read-file-name-completion-ignore-case t)
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; 操作に関する設定
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;; スクロールマウスを有効
 (global-set-key [mouse-4] 'scroll-down)
 (global-set-key [mouse-5] 'scroll-up)
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-; KeyBinding
+;; KeyBinding
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (setq mac-option-modifier 'meta)
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; evil KeyBinding
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;(setcdr evil-insert-state-map nil)
+(define-key evil-insert-state-map [escape] 'evil-normal-state)
+(key-chord-define evil-insert-state-map "jj" 'evil-normal-state)
+
+
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; rspec-mode
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(require 'rspec-mode)
+(eval-after-load 'rspec-mode
+    '(rspec-install-snippets))
+(custom-set-variables '(rspec-use-rake-flag nil))
+(custom-set-faces )
+
 
 
