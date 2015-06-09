1895bc965fcc3943d395bc4612e8f7bb3e4a3bb8
Shota Miyamoto <miyamoto@feedforce.jp>
2015-06-09 15:49:59 +0900

    Add emacs setting

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -54,14 +54,6 @@
 (define-key helm-find-files-map (kbd "TAB") 'helm-execute-persistent-action)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;; flycheck
-(require 'flycheck)
-(require 'flyspell)
-(flycheck-mode 1)
-(flyspell-mode 1)
-(add-hook 'after-init-hook #'global-flycheck-mode)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; auto-complete
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (require 'auto-complete)
@@ -205,6 +197,8 @@
 (custom-set-variables '(rspec-use-rake-flag nil))
 (custom-set-faces )
 
+(require 'rubocop)
+(add-hook 'ruby-mode-hook 'rubocop-mode)
 
 
 
@@ -213,7 +207,40 @@
 
 
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; flycheck
+(require 'flycheck)
+(require 'flyspell)
+(flycheck-mode 1)
+(flyspell-mode 1)
+(add-hook 'after-init-hook #'global-flycheck-mode)
+;; (flycheck-define-checker ruby-rubocop
+;;   "A Ruby syntax and style checker using the RuboCop tool."
+;;   :command ("rubocop" "--format" "emacs" "--silent"
+;;             (config-file "--config" flycheck-rubocoprc)
+;;             source)
+;;   :error-patterns
+;;   ((warning line-start
+;;             (file-name) ":" line ":" column ": " (or "C" "W") ": " (message)
+;;             line-end)
+;;    (error line-start
+;;           (file-name) ":" line ":" column ": " (or "E" "F") ": " (message)
+;;           line-end))
+;;      :modes (enh-ruby-mode motion-mode))
 
+
+;; definition for flycheck
+(flycheck-define-checker ruby-rubylint
+  "A Ruby syntax and style checker using the rubylint tool."
+  :command ("ruby-lint" source)
+  :error-patterns
+  ((warning line-start
+            (file-name) ":" line ":" column ": " (or "C" "W") ": " (message)
+            line-end)
+   (error line-start
+          (file-name) ":" line ":" column ": " (or "E" "F") ": " (message)
+          line-end))
+    :modes (enh-ruby-mode ruby-mode))
 
 
 
