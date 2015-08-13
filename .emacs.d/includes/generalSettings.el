66b90a95134b2d9ceeda250039c83afc9c76ef6d
Shota Miyamoto <miyamoto@feedforce.jp>
2015-08-13 16:23:09 +0900

    hoge

--- .emacs.d/includes/generalSettings.el
+++ .emacs.d/includes/generalSettings.el
@@ -34,3 +34,19 @@
 
 (provide 'generalSettings)
 ;;; generalSettings.el ends here
+
+;;; Get current path and put it to clipboard
+(defun put-current-path-to-clipboard ()
+  (interactive)
+  (let ((file-path buffer-file-name)
+        (dir-path default-directory))
+    (cond (file-path
+           (kill-new (expand-file-name file-path))
+           (message "This file path is on the clipboard!"))
+          (dir-path
+           (kill-new (expand-file-name dir-path))
+           (message "This directory path is on the clipboard!"))
+          (t
+           (error-message-string "Fail to get path name.")
+           ))))
+(global-set-key (kbd "C-c C-c p") 'put-current-path-to-clipboard)
