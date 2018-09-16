c5eb2752f7e2b31ccb755624f902d65b89cdbbc9
surume <sururne@gmail.com>
2018-09-17 05:46:56 +0900

    Remove unnecessary alias exports

--- .config/fish/env.fish
+++ .config/fish/env.fish
@@ -3,26 +3,14 @@ set -x TERM "xterm-256color"
 
 set -x EDITOR 'vim'
 set -x GOPATH $HOME/src
-set -x CARGO_PATH $HOME/.cargo
-set -x JAVA_HOME (/usr/libexec/java_home -v 1.8)
-set -x VAGRANT_HOME $HOME
 set -x DIFF_HIGHLIGHT_PATH /usr/local/share/git-core/contrib/diff-highlight
 # set -x ANDROID_HOME=/usr/local/opt/android-sdk
-# set -x JOHN_HOME=/usr/local/Cellar/john-jumbo/1.8.0/share/john
 
 set -x PATH /usr/local/bin $PATH
 set -x PATH $GOPATH/bin $PATH
-set -x PATH $CARGO_PATH/bin $PATH
 set -x PATH $DIFF_HIGHLIGHT_PATH $PATH
-# set -x PATH $JOHN_HOME $PATH
 
 set -x MY_GITHUB_ROOT (ghq root)/github.com/(git config --get user.name)
-set -x YARN_GLBL_PATH (yarn global bin)
-set -x YARN_GLBL_PATH2 ~/.config/yarn/global/node_modules/.bin
-set -x PATH $YARN_GLBL_PATH $PATH
-set -x PATH $YARN_GLBL_PATH2 $PATH
-set -x NPM_BIN_PATH ~/.npm-packages/bin
-set -x PATH $NPM_BIN_PATH $PATH
 
 
 # direnv
