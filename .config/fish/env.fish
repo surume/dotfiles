727b7bb110598216dd47796d40bab582e541f886
surume <sururne@gmail.com>
2019-09-10 16:15:47 +0900

    Update mysql-client install comannd

--- .config/fish/env.fish
+++ .config/fish/env.fish
@@ -9,6 +9,7 @@ set -x DIFF_HIGHLIGHT_PATH /usr/local/share/git-core/contrib/diff-highlight
 set -x PATH /usr/local/bin $PATH
 set -x PATH $GOPATH/bin $PATH
 set -x PATH $DIFF_HIGHLIGHT_PATH $PATH
+set -x PATH /usr/local/opt/mysql-client/bin $PATH
 
 set -x MY_GITHUB_ROOT (ghq root)/github.com/(git config --get user.name)
 
--- scripts/setup/brew.sh
+++ scripts/setup/brew.sh
@@ -29,7 +29,7 @@ cli_apps=(
     kubectx
     m-cli
     mas
-    '"mysql --client-only"'
+    mysql-client
     neovim
     newt
     openssl
