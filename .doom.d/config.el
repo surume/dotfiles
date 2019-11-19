2a706d3faffb6754918a9390e9bdfeabfdf1fe9f
surume <sururne@gmail.com>
2019-11-19 22:52:50 +0900

    Add doom.d

---
+++ .doom.d/config.el
@@ -0,0 +1,63 @@
+;;; .doom.d/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here
+;; meta
+(setq mac-command-modifier 'super)
+(setq mac-option-modifier 'alt)
+(setq mac-right-command-modifier 'super)
+(setq mac-right-option-modifier 'hyper)
+
+;; コメントを見やすく
+(setq doom-one-brighter-comments t)
+;; don't lighten the background of the comment
+(setq doom-one-comment-bg nil)
+
+;; ウィンドウ移動
+;; (define-key evil-visual-state-map (kbd "C-k") 'evil-window-up)
+;; (define-key evil-visual-state-map (kbd "C-j") 'evil-window-down)
+;; (define-key evil-visual-state-map (kbd "C-h") 'evil-window-left)
+;; (define-key evil-visual-state-map (kbd "C-l") 'evil-window-right)
+
+;; 補完設定
+(global-company-mode)
+(define-key company-active-map (kbd "C-n") 'company-select-next)
+(define-key company-active-map (kbd "C-p") 'company-select-previous);; Keybind
+
+;; 自動保存設定
+(setq auto-save-buffers-enhanced-interval 1.0)
+(auto-save-buffers-enhanced t)
+
+;; プロジェクト/ファイル移動
+;; (define-key global-map (kbd "C-c h") 'projectile-switch-project)
+(require 'ivy-ghq)
+(setq ivy-ghq-short-list t)
+(define-key global-map (kbd "s-g") 'ivy-ghq-open)
+(define-key global-map (kbd "s-p") 'projectile-find-file)
+
+;; jj, kk で ESC
+(key-chord-mode 1)
+(setq key-chord-two-keys-delay 0.6)
+(key-chord-define evil-insert-state-map  "jj" 'evil-normal-state)
+(key-chord-define evil-insert-state-map  "kk" 'evil-normal-state)
+
+;; 定義元ジャンプ
+(define-key global-map (kbd "s-b") 'dumb-jvump-go)
+(define-key global-map (kbd "s-[") 'dumb-jump-back)
+(define-key global-map (kbd "s-B") 'dumb-jump-go-other-window)
+
+;; elscreen設定
+;; (setq elscreen-prefix-key (kbd "s-e"))
+;; (elscreen-start)
+;; (define-key global-map (kbd "s-n") 'elscreen-create)
+;; (define-key global-map (kbd "s-t") 'elscreen-create)
+;; (define-key global-map (kbd "s-w") 'elscreen-kill)
+;; (define-key global-map (kbd "s-{") 'elscreen-previous)
+;; (define-key global-map (kbd "s-}") 'elscreen-next)
+
+;; JSのセミコロンチェックを殺す
+(setq js2-strict-missing-semi-warning nil)
+(setq js2-missing-semi-one-line-override nil)
+
+;; (custom-set-variables
+;;   '(google-translate-default-source-language "en")
+;; '(google-translate-default-target-language "ja"))
---
+++ .doom.d/packages.el
@@ -0,0 +1,10 @@
+;; -*- no-byte-compile: t; -*-
+;;; .doom.d/packages.el
+
+;;; Examples:
+(package! auto-save-buffers-enhanced)
+(package! key-chord)
+(package! ivy-ghq :recipe (:host github :repo "analyticd/ivy-ghq"))
+;; (package! some-package)
+;; (package! another-package :recipe (:host github :repo "username/repo"))
+;; (package! builtin-package :disable t)
