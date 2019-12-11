53135d91add088028def65304bfcce0e4e113311
surume <sururne@gmail.com>
2019-12-11 18:38:26 +0900

    Use docker BUILDKIT

--- .config/fish/env.fish
+++ .config/fish/env.fish
@@ -12,7 +12,8 @@ set -x PATH $DIFF_HIGHLIGHT_PATH $PATH
 set -x PATH /usr/local/opt/mysql-client/bin $PATH
 
 set -x MY_GITHUB_ROOT (ghq root)/github.com/(git config --get user.name)
-
+set -x DOCKER_BUILDKIT 1
+set -x COMPOSE_DOCKER_CLI_BUILD 1
 
 # direnv
 direnv hook fish | source
