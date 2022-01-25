6577add5842eb7f10de88ac1a8a21f5989f7746b
surume <sururne@gmail.com>
2022-01-25 20:56:43 +0900

    ♻️refactor: scripts -> bin

--- scripts/cron/cleanDesktop.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-
-if ls ~/Desktop/* > /dev/null 2>&1; then
-    mv ~/Desktop/* ~/Documents/backup
-fi
--- scripts/cron/updateBrews.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-logPath=/tmp/brewUpdate.log
-/bin/date >> $logPath
-/usr/local/bin/brew update >> $logPath
-/usr/local/bin/brew upgrade >> $logPath
-/usr/local/bin/brew cleanup >> $logPath
