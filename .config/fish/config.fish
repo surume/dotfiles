11b4672102950d4313f5650660891110b546f4a7
surume <sururne@gmail.com>
2025-01-02 15:52:10 +0900

    chore: update fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,7 +5,20 @@ if status --is-login
   . ~/.config/fish/aliases.fish
 end
 
-. ~/.local/share/fish/config.fish
+set -g PATH /opt/homebrew/bin $PATH
+set -x PATH $HOME/.local/bin $PATH
+set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
 set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
 set -g fish_user_paths "/usr/local/opt/gettext/bin" $fish_user_paths
 starship init fish | source
+
+# pnpm
+set -gx PNPM_HOME "/Users/shotamiyamoto/Library/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
+source /opt/homebrew/opt/asdf/libexec/asdf.fish
+
+# Created by `pipx` on 2024-09-26 12:14:40
+set PATH $PATH /Users/shotamiyamoto/.local/bin
--- .config/fish/env.fish
+++ .config/fish/env.fish
@@ -13,16 +13,24 @@ set -x PATH /opt/homebrew/share/git-core/contrib/diff-highlight/ $PATH
 set -x PATH $GOPATH/bin $PATH
 set -x PATH $DIFF_HIGHLIGHT_PATH $PATH
 set -x PATH /usr/local/opt/mysql-client/bin $PATH
+set -x PATH /opt/homebrew/opt/mysql-client/bin $PATH
 set -x PATH /usr/local/bin $PATH
 set -x PATH /usr/bin $PATH
 set -x PATH /bin $PATH
 set -x PATH /usr/sbin $PATH
 set -x PATH /sbin $PATH
+set -x PATH /opt/homebrew/opt/openjdk@11/bin $PATH
 set -x PATH $PATH $HOME/.krew/bin
 
 set -x MY_GITHUB_ROOT (ghq root)/github.com/(git config --get user.name)
 set -x DOCKER_BUILDKIT 1
 set -x COMPOSE_DOCKER_CLI_BUILD 1
+set -x USE_GKE_GCLOUD_AUTH_PLUGIN True
+set -x LOCAL_IP_ADDRESS (ifconfig | grep inet | cut -d " " -f2 | grep "[0-9]*\.[0-9]*\.[0-9]*\.[0-9]*" | awk 'NR==2')
+
+mise activate fish | source
+# mise hook-env -s fish | source  # プラグインのパスを追加する
+mise complete -s fish | source
 
 # direnv
 direnv hook fish | source
