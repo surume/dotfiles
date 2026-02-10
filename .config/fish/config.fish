51761eaa6e12f7dec556a5d8045ba4106bc64268
surume <sururne@gmail.com>
2026-02-10 11:57:13 +0900

    feat: add configuration for new tools and integrations (#48)
    * chore: update default and fallback models
    
    * chore: add OrbStack integration
    
    * feat: add configuration for new tools and integrations

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -26,3 +26,4 @@ set PATH $PATH /Users/shotamiyamoto/.local/bin
 # Added by OrbStack: command-line tools and integration
 # This won't be added again if you remove it.
 source ~/.orbstack/shell/init2.fish 2>/dev/null || :
+
