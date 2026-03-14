ef80475c5a30f9827b490fd1905e30954bd9a85e
surume <sururne@gmail.com>
2026-03-15 02:30:57 +0900

    refactor(fish): modernize PATH setup and remove TERM override
    - Replace repetitive set -x PATH with fish_add_path
    - Remove TERM override (terminal emulator sets this correctly)
    - Remove Intel Mac (/usr/local) paths
    - Remove DIFF_HIGHLIGHT_PATH variable (inlined)

--- .config/fish/env.fish
+++ .config/fish/env.fish
@@ -1,23 +1,14 @@
 # Env path ================================================
-set -x TERM "xterm-256color"
-
 set -x EDITOR 'vim'
 set -x GOPATH $HOME
-set -x DIFF_HIGHLIGHT_PATH /usr/local/share/git-core/contrib/diff-highlight
-set -x PATH /usr/local/bin $PATH
-set -x PATH /opt/homebrew/bin $PATH
-set -x PATH /opt/homebrew/sbin $PATH
-set -x PATH /opt/homebrew/share/git-core/contrib/diff-highlight/ $PATH
-set -x PATH $GOPATH/bin $PATH
-set -x PATH $DIFF_HIGHLIGHT_PATH $PATH
-set -x PATH /usr/local/opt/mysql-client/bin $PATH
-set -x PATH /opt/homebrew/opt/mysql-client/bin $PATH
-set -x PATH /usr/bin $PATH
-set -x PATH /bin $PATH
-set -x PATH /usr/sbin $PATH
-set -x PATH /sbin $PATH
-set -x PATH /opt/homebrew/opt/openjdk@11/bin $PATH
-set -x PATH $PATH $HOME/.krew/bin
+
+fish_add_path /opt/homebrew/bin
+fish_add_path /opt/homebrew/sbin
+fish_add_path /opt/homebrew/share/git-core/contrib/diff-highlight
+fish_add_path /opt/homebrew/opt/mysql-client/bin
+fish_add_path /opt/homebrew/opt/openjdk@11/bin
+fish_add_path $GOPATH/bin
+fish_add_path $HOME/.krew/bin
 
 set -x MY_GITHUB_ROOT (ghq root)/github.com/(git config --get user.name)
 set -x MAS_NO_AUTO_INDEX 1
