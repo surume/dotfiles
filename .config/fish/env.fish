a746c162f491435b20829eb0319b211bab598ae1
surume <sururne@gmail.com>
2026-03-15 02:43:24 +0900

    refactor(fish): remove redundant env vars
    - GOPATH=$HOME is Go default, use $HOME/go/bin directly
    - DOCKER_BUILDKIT=1 is default since Docker 23.0+
    - COMPOSE_DOCKER_CLI_BUILD=1 is default in Compose v2
    - Remove commented-out mise hook-env

--- .config/fish/env.fish
+++ .config/fish/env.fish
@@ -1,25 +1,19 @@
 # Env path ================================================
 set -x EDITOR 'vim'
-set -x GOPATH $HOME
 
 fish_add_path /opt/homebrew/bin
 fish_add_path /opt/homebrew/sbin
 fish_add_path /opt/homebrew/share/git-core/contrib/diff-highlight
 fish_add_path /opt/homebrew/opt/mysql-client/bin
 fish_add_path /opt/homebrew/opt/openjdk@11/bin
-fish_add_path $GOPATH/bin
+fish_add_path $HOME/go/bin
 fish_add_path $HOME/.krew/bin
 
 set -x MY_GITHUB_ROOT (ghq root)/github.com/(git config --get user.name)
 set -x MAS_NO_AUTO_INDEX 1
-set -x DOCKER_BUILDKIT 1
-set -x COMPOSE_DOCKER_CLI_BUILD 1
 set -x USE_GKE_GCLOUD_AUTH_PLUGIN True
 set -x LOCAL_IP_ADDRESS (ifconfig | grep inet | cut -d " " -f2 | grep "[0-9]*\.[0-9]*\.[0-9]*\.[0-9]*" | awk 'NR==2')
 
 mise activate fish | source
-# mise hook-env -s fish | source  # プラグインのパスを追加する
 mise complete -s fish | source
-
-# direnv
 direnv hook fish | source
