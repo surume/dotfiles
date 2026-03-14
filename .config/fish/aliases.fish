21f22105340c6fb4b9e8280bf8dfa4d4febc4581
surume <sururne@gmail.com>
2026-03-15 02:44:46 +0900

    refactor(fish): reorganize aliases.fish
    - Group aliases together, functions together
    - Remove commented-out dst alias
    - Remove misleading section comments

--- .config/fish/aliases.fish
+++ .config/fish/aliases.fish
@@ -1,20 +1,26 @@
-# ssh ============================================================================
+# Aliases
+alias g git
+alias d docker
+alias fig 'docker compose'
+alias k kubecolor
+alias ls eza
+alias vi nvim
+alias vim nvim
+alias find fd
+alias gip 'curl globalip.me'
+alias be 'bundle exec'
+
+# Functions
 function ss
     cat ~/.ssh/config ~/.ssh/conf.d/local.nosync/confi* | grep "^Host " | egrep -v 'github.com|bitbucket.org' | sed s/"Host "// | sort | fzf | read -l sshhostname
     sshrc -A $sshhostname
 end
 
 function dssh
-    docker ps --format "table {{.ID}} {{.Names}}" | grep -v "CONTAINER"| fzf | awk '{print $1}' | read -l container_id
+    docker ps --format "table {{.ID}} {{.Names}}" | grep -v "CONTAINER" | fzf | awk '{print $1}' | read -l container_id
     docker exec -it $container_id /bin/bash
 end
 
-# git ============================================================================
-alias g git
-alias ls eza
-alias vi nvim
-alias vim nvim
-alias find fd
 function mkrepo
   set -lx repo $MY_GITHUB_ROOT/$argv
   mkdir -p $repo
@@ -25,15 +31,3 @@ end
 function rmrepo
   ghq list -p | fzf -m | xargs -I{} rm -rf {}
 end
-
-# docker =========================================================================
-alias d docker
-alias fig 'docker compose'
-alias k kubecolor
-# alias dst 'docker stats $(docker ps | grep -v "NAMES" | awk '{ print $NF }' | tr "\n" " ")'
-
-# global ip ======================================================================
-alias gip 'curl globalip.me'
-
-# bundle exec ====================================================================
-alias be 'bundle exec'
