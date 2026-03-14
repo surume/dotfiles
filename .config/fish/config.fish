1b4300cd30bcf867366369dd043251510266e5ff
surume <sururne@gmail.com>
2026-03-15 02:43:06 +0900

    refactor(fish): clean up config.fish
    - Remove unused bobthefish theme_color_scheme (using starship)
    - Replace pnpm PATH block with fish_add_path
    - Remove unnecessary OrbStack comment

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,20 +1,9 @@
-set -g theme_color_scheme zenburn
-
 if status --is-login
   . ~/.config/fish/env.fish
   . ~/.config/fish/aliases.fish
 end
 
 starship init fish | source
-
-# pnpm
-set -gx PNPM_HOME "$HOME/Library/pnpm"
-if not string match -q -- $PNPM_HOME $PATH
-  set -gx PATH "$PNPM_HOME" $PATH
-end
-# pnpm end
-
-# Added by OrbStack: command-line tools and integration
-# This won't be added again if you remove it.
+fish_add_path "$HOME/Library/pnpm"
 source ~/.orbstack/shell/init2.fish 2>/dev/null || :
 
