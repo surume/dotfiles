8502e174c8c1af61bfcc3d6f3c3ff2d770c77ac2
surume <sururne@gmail.com>
2026-03-14 23:13:25 +0900

    fix(fish): remove unused asdf source (migrated to mise)

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -15,7 +15,6 @@ if not string match -q -- $PNPM_HOME $PATH
   set -gx PATH "$PNPM_HOME" $PATH
 end
 # pnpm end
-source /opt/homebrew/opt/asdf/libexec/asdf.fish
 
 # Added by OrbStack: command-line tools and integration
 # This won't be added again if you remove it.
